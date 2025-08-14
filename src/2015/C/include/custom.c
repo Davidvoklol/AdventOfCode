f2bb014bf258772c257bf57449d5ece034e7a216
lobster <polyak.daavid@gmail.com>
2025-08-15 01:11:27 +0200

    2015/C: ...

---
+++ src/2015/C/include/custom.c
@@ -0,0 +1,10 @@
+#include "custom.h"
+#include <stdio.h>
+
+void checkinput(int argc, char* argv[]) {
+	if (argc < 2) {
+		printf("No input were provided...\n");
+		printf("Expected: %s [input]\n", argv[0]);
+		exit(1);
+	}
+}
---
+++ src/2015/C/include/custom.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void checkinput(int argc, char* argv[]);
