63c835e193680916432b4dbca9cd895d2bd97ffb
lobster <polyak.daavid@gmail.com>
2025-08-15 12:23:22 +0200

    2015/C: ...

--- src/2015/C/include/custom.c
+++ src/2015/C/include/custom.c
@@ -1,7 +1,7 @@
 #include "custom.h"
 #include <stdio.h>
 
-void checkinput(int argc, char* argv[]) {
+void checkinput(int argc, char *argv[]) {
 	if (argc < 2) {
 		printf("No input were provided...\n");
 		printf("Expected: %s [input]\n", argv[0]);
--- src/2015/C/include/custom.h
+++ src/2015/C/include/custom.h
@@ -3,4 +3,4 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void checkinput(int argc, char* argv[]);
+void checkinput(int argc, char *argv[]);
