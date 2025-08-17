536f4849bf7b943b0dd2acef1c1c330d0c9f22f4
lobster <polyak.daavid@gmail.com>
2025-08-17 11:46:37 +0200

    2015/C/day1/part2: complete

--- src/2015/C/day1/part2/main.c
+++ src/2015/C/day1/part2/main.c
@@ -3,10 +3,36 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-	checkinput(argc, argv);
+	if (checkinput(argc, argv) == FAIL) exit(1);
 
 	FILE *input = verbose_fopen(argv[1], "r");
 	if (!input) exit(1);
 
+	int floor = 0;
+	int index = 0;
+	while (1 && floor >= 0) {
+		char buffer[420];
+
+		char *success = fgets(buffer, sizeof(buffer), input);
+		if (!success)  break;
+
+		for (char *current_ptr = buffer, *end = buffer + sizeof(buffer); current_ptr < end && *current_ptr != '\0'; current_ptr++) {
+			index++;
+
+			char current = *current_ptr;
+			if (current == '\n') continue;
+
+			if (current == '(') floor++;
+			else if (current == ')') floor--;
+
+			if (floor < 0) break;
+		}
+	}
+
+	if (floor < 0) printf("answer: %i\n", index);	
+	else printf("no answer found");
+
 	fclose(input);
 }
+
+
