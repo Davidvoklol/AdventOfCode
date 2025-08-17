d58ad467b5fe235bd7ca787834209c1a9b06cb7b
lobster <polyak.daavid@gmail.com>
2025-08-17 21:03:55 +0200

    2015/C: ...

--- src/2015/C/day2/part1/main.c
+++ src/2015/C/day2/part1/main.c
@@ -1,5 +1,6 @@
 #include "../../include/custom.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
 	if (checkinput(argc, argv) == FAIL) exit(1);
@@ -7,15 +8,8 @@ int main(int argc, char *argv[]) {
 	FILE *input = verbose_fopen(argv[1], "r");
 	if (!input) exit(1);
 
-	int index = 0;
-	char buffer[4];
 	while (1) {
-		buffer[0] = '\0';
-
-		char *success = fgets(buffer, sizeof(buffer), input);
-		if (!success)  break;
-
-		printf("%i: [%s]\n", index, buffer);
-		index++;
 	}
+
+	fclose(input);
 }
